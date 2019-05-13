a4dab6b076afecd4c08f47d18fb12a9e2cf4dc87
Vincenzo Pandolfo <pandolfovince@gmail.com>
2019-05-13 20:57:59 +0100

    Added function time access and opsdat

--- devito/ops/types.py
+++ devito/ops/types.py
@@ -11,3 +11,27 @@ class Array(basic.Array):
     @property
     def _C_name(self):
         return self.name
+
+
+class FunctionTimeAccess(basic.Basic):
+    is_LocalObject = True
+    is_ArrayAccess = True
+
+    def __init__(self, function, time_access_symbol):
+        self.function = function
+        self.time_access_symbol = time_access_symbol
+
+    @property
+    def _C_name(self):
+        return "%s[%s]" % (self.function.name, self.time_access_symbol)
+
+    @property
+    def free_symbols(self):
+        return [self.function]
+
+
+class OPSDat(basic.Symbol):
+    is_Symbol = True
+
+    def get_decl_pair(self):
+        return ["ops_dat"], self.name
