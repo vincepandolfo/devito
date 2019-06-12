0070fc52e53ac2161479a1481b47cd13cb0de26e
Vincenzo Pandolfo <pandolfovince@gmail.com>
2019-06-12 23:56:22 +0100

    New try - compiling openmp ops from Devito

--- devito/ops/compiler.py
+++ devito/ops/compiler.py
@@ -16,16 +16,24 @@ class OPSOpenMPCompiler(Compiler):
     MPICC = os.environ.get('MPICC', 'mpicc')
     MPICXX = os.environ.get('MPICXX', 'mpicxx')
 
-    def __init__(self, kernel_name, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        kwargs['cpp'] = True
+        kwargs['mpi'] = True
         super(OPSOpenMPCompiler, self).__init__(*args, **kwargs)
         ops_install_path = os.environ.get('OPS_INSTALL_PATH')
-        default = ('-O3 -g -march=native -fPIC -Wall -I%s/c/include -L%s/c/lib' %
-                   (ops_install_path, ops_install_path))
+        default = '-O3 -g -DUNIX -ffloat-store -fPIC -Wall'
         self.cflags = os.environ.get('CFLAGS', default).split(' ')
-        self.ldflags = os.environ.get('LDFLAGS', '-shared -fopenmp -lstdc++').split(' ')
 
-        self.ldflags += ('-I%s %s/MPI_OpenMP/%s -lops_seq' % (
-            get_jit_dir(), get_jit_dir(), kernel_name)).split(' ')
+        self.ldflags = os.environ.get('LDFLAGS', '-shared -fopenmp').split(' ')
+
+        include_dirs = '%s %s/c/include' % (get_jit_dir(), ops_install_path)
+        self.include_dirs = include_dirs.split(' ')
+
+        library_dirs = '%s/c/lib' % ops_install_path
+        self.library_dirs = library_dirs.split(' ')
+
+        libraries = "ops_seq stdc++"
+        self.libraries = libraries.split(' ')
 
     def __lookup_cmds__(self):
         self.CC = 'gcc'
@@ -105,8 +113,12 @@ def jit_compile(soname, code, h_code, compiler):
         # removing generated cuda kernels to avoid reuse
         subprocess.run(["rm -rf ./CUDA"], cwd=get_jit_dir(), shell=True)
     elif configuration.ops['target'] == 'OpenMP':
-        omp_kernel_name = '%s_omp_kernels.cpp' % soname
-        compiler = OPSOpenMPCompiler(omp_kernel_name)
+        omp_kernel = '%s/MPI_OpenMP/%s_omp_kernels.cpp' % (get_jit_dir(), soname)
+        omp_code = ""
+        with open(omp_kernel, 'r') as f:
+            omp_code = f.read()
+
+        compiler = OPSOpenMPCompiler()
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
 
@@ -114,12 +126,15 @@ def jit_compile(soname, code, h_code, compiler):
             # Spinlock in case of MPI
             sleep_delay = 0 if configuration['mpi'] else 1
             _, _, _, recompiled = compile_from_string(
-                compiler, target, code, src_file,
+                compiler, target, [code, omp_code],
+                [src_file, omp_kernel],
                 cache_dir=cache_dir,
                 debug=configuration['debug-compiler'],
                 sleep_delay=sleep_delay
             )
             toc = time()
+
+        subprocess.run(["rm -rf ./MPI_OpenMP"], cwd=get_jit_dir(), shell=True)
 
         if recompiled:
             debug("%s: compiled `%s` [%.2f s]" % (compiler, src_file, toc-tic))
