c0cfe156405b3c624ffe862f4ba138661a89cace
Vincenzo Pandolfo <pandolfovince@gmail.com>
2019-06-13 00:43:39 +0100

    2step cuda compilation

--- devito/ops/compiler.py
+++ devito/ops/compiler.py
@@ -1,5 +1,5 @@
 from time import time
-from codepy.jit import compile_from_string
+from codepy.jit import compile_from_string, link_extension
 
 import os
 import subprocess
@@ -33,6 +33,61 @@ class OPSOpenMPCompiler(Compiler):
         self.library_dirs = library_dirs.split(' ')
 
         libraries = "ops_seq stdc++"
+        self.libraries = libraries.split(' ')
+
+    def __lookup_cmds__(self):
+        self.CC = 'gcc'
+        self.CXX = 'g++'
+        self.MPICC = 'mpicc'
+        self.MPICXX = 'mpicxx'
+
+
+class OPSCUDACompiler(Compiler):
+    CC = os.environ.get('CC', 'nvcc')
+
+    def __init__(self, *args, **kwargs):
+        super(OPSOpenMPCompiler, self).__init__(*args, **kwargs)
+
+        ops_install_path = os.environ.get('OPS_INSTALL_PATH')
+        cuda_install_path = os.environ.get('CUDA_INSTALL_PATH')
+
+        self.cc = '%s/bin/nvcc' % cuda_install_path
+
+        self.cflags = ['-O3', '-XCompiler="-std=c99 -fPIC"']
+        self.ldflags = []
+
+        include_dirs = '%s %s/c/include' % (get_jit_dir(), ops_install_path)
+        self.include_dirs = include_dirs.split(' ')
+
+    def __lookup_cmds__(self):
+        self.CC = 'nvcc'
+
+
+class OPSCudaStep2Compiler(Compiler):
+    CC = os.environ.get('CC', 'gcc')
+    CXX = os.environ.get('CXX', 'g++')
+    MPICC = os.environ.get('MPICC', 'mpicc')
+    MPICXX = os.environ.get('MPICXX', 'mpicxx')
+
+    def __init__(self, *args, **kwargs):
+        kwargs['cpp'] = True
+        kwargs['mpi'] = True
+        super(OPSOpenMPCompiler, self).__init__(*args, **kwargs)
+        ops_install_path = os.environ.get('OPS_INSTALL_PATH')
+        cuda_install_path = os.environ.get('CUDA_INSTALL_PATH')
+
+        cflags = '-O3 -shared -fopenmp -fPIC -Wall'
+        self.cflags = os.environ.get('CFLAGS', cflags).split(' ')
+
+        self.ldflags = os.environ.get('LDFLAGS', '').split(' ')
+
+        include_dirs = '%s/include %s/c/include' % (cuda_install_path, ops_install_path)
+        self.include_dirs = include_dirs.split(' ')
+
+        library_dirs = '%s/lib64 %s/c/lib' % (cuda_install_path, ops_install_path)
+        self.library_dirs = library_dirs.split(' ')
+
+        libraries = "ops_cuda cudart"
         self.libraries = libraries.split(' ')
 
     def __lookup_cmds__(self):
@@ -81,34 +136,52 @@ def jit_compile(soname, code, h_code, compiler):
         "%s.cpp" % soname
     ], cwd=get_jit_dir())
 
+    ops_src = '%s/%s_ops.cpp' % (get_jit_dir(), soname)
+    with open(ops_src, 'r') as f:
+        code = f.read()
+
     if configuration.ops['target'] == 'CUDA':
         # CUDA kernel compilation
-        cuda_install_path = os.environ.get("CUDA_INSTALL_PATH")
-        subprocess.run([' '.join([
-            '%s/bin/nvcc' % cuda_install_path,
-            '-Xcompiler="-std=c99 -fPIC"',
-            '-O3',
-            '-gencode arch=compute_60,code=sm_60',
-            '-I%s/c/include' % ops_install_path,
-            '-I.',
-            '-c',
-            '-o ./CUDA/%s_kernels_cu.o' % soname,
-            './CUDA/%s_kernels.cu' % soname
-        ])], cwd=get_jit_dir(), shell=True)
+        cuda_compiler = OPSCUDACompiler()
+        cuda_src = '%s/CUDA/%s_kernels.cu' % (get_jit_dir(), soname)
+        cuda_target = '%s/%s_kernels_cu' % (get_jit_dir(), soname)
 
-        subprocess.run([' '.join([
-            'g++',
-            '-fopenmp -O3 -shared -fPIC -Wall -g',
-            '-march=native',
-            '-I%s/include' % cuda_install_path,
-            '-I%s/c/include' % ops_install_path,
-            '-L%s/c/lib' % ops_install_path,
-            '-L%s/lib64' % cuda_install_path,
-            '%s_ops.cpp' % soname,
-            './CUDA/%s_kernels_cu.o' % soname,
-            '-lcudart -lops_cuda',
-            '-o %s.so' % soname
-        ])], cwd=get_jit_dir(), shell=True)
+        cuda_code = ""
+        with open(cuda_src, 'r') as f:
+            cuda_code = f.read()
+
+        cuda_step2_compiler = OPSCudaStep2Compiler()
+
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+
+            # Spinlock in case of MPI
+            sleep_delay = 0 if configuration['mpi'] else 1
+            _, _, cuda_o, recompiled = compile_from_string(
+                cuda_compiler, cuda_target,
+                cuda_code, cuda_src,
+                cache_dir=cache_dir,
+                debug=configuration['debug-compiler'],
+                sleep_delay=sleep_delay,
+                object=True
+            )
+
+            _, _, src_o, recompiled = compile_from_string(
+                cuda_step2_compiler, target,
+                code, src_file,
+                cache_dir=cache_dir,
+                debug=configuration['debug-compiler'],
+                sleep_delay=sleep_delay,
+                object=True
+            )
+
+            link_extension(
+                cuda_step2_compiler,
+                [cuda_o, src_o],
+                target,
+                cache_dir=cache_dir,
+                debug=configuration['debug-compiler']
+            )
 
         # removing generated cuda kernels to avoid reuse
         subprocess.run(["rm -rf ./CUDA"], cwd=get_jit_dir(), shell=True)
